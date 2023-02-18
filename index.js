5ad0b08ff1f303ee1264331be0b0c4f6df9b27b3
bethimanideep <bethimanideep1999@gmail.com>
2023-02-18 22:30:48 +0530

    full

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express()
 const cors = require("cors");
 require("dotenv").config()
 const {router}=require("./routes/user.route")
-app.use(cors())
+app.use(
+  cors({
+    orgin: ["http://127.0.0.1:5500"],
+  }),
+);
 app.use(express.json())
 app.use("/",router)
 app.listen(process.env.port, async (req, res) => {

d78ebdf3681461fb1ecb0376cf3a8c416acdc66b
bethimanideep <bethimanideep1999@gmail.com>
2023-02-18 22:55:00 +0530

    full

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const { connection} = require('./db');
+const { connection } = require('./db');
 const app = express()
 const cors = require("cors");
 require("dotenv").config()
-const {router}=require("./routes/user.route")
-app.use(
-  cors({
-    orgin: "https://lucky-parfait-82d7c2.netlify.app",
-  }),
-);
+const { router } = require("./routes/user.route")
+app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+    origin: 'https://lucky-parfait-82d7c2.netlify.app/',
+    optionsSuccessStatus: 200
+};
+
+app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));
+
 app.use(express.json())
-app.use("/",router)
+app.use("/", router)
 app.listen(process.env.port, async (req, res) => {
     try {
         await connection
